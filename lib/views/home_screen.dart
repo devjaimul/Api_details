917fbb1e82266689348dd4cb26b7976e7f10175d
Jaimul Islam Akik <105303801+devjaimul@users.noreply.github.com>
2024-08-29 23:38:43 +0600

    updated

--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -1,13 +1,53 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
-class HomeScreen extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<dynamic> posts = [];
+  Future fetchData() async {
+    final url = Uri.parse('https://jsonplaceholder.typicode.com/posts');
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      print('success');
+      final jsonData = jsonDecode(response.body);
+
+      final postList = jsonData as List;
+      posts = postList;
+      print(postList.length);
+    }
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    fetchData();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Api'),
+        title: const Text('Api'),
+      ),
+      body: ListView.builder(
+        itemCount: posts.length,
+        itemBuilder: (context, index) {
+          return Card(
+            child: ListTile(
+              title: Text(posts[index]['title']),
+            ),
+          );
+        },
       ),
     );
   }
